9eb67a2f6636b14e37be87059ce71aea6d360420
Hazemyahea <hazemyahea1@gmail.com>
2024-06-12 01:13:22 +0300

    g

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://mskgzkslahrdrqakhwao.supabase.co";
+console.log(supabaseUrl);
 const supabaseKey = import.meta.env.VITE_API_KEY;
-
+console.log(supabaseKey);
 export const supabase = createClient(supabaseUrl, supabaseKey);
